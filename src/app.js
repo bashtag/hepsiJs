5289ddb0e9a2f97de6c82e4a5490bd8a34b60d16
bashtag <btstsiy@hotmail.com>
2023-07-15 16:56:19 +0300

    main js file

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import SignBtn from "./components/signBtn/sign.js";
+import SwitchBtn from "./components/signRadioBtn/switch.js";
+import Hash from "./modules/authantication/hash.js";
+import Page from "./modules/page.js";
+
+// Page Action Infos
+let	pageActionInfo = new Page();
+
+// Radio Button Attribute
+let	radioBtnAttribute = new SwitchBtn(pageActionInfo);
+
+// Sign Button
+// let	signButtonAction = new SignBtn(pageActionInfo);
