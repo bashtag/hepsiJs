530b526a30827791b3a994219d80c5aa8794b538
bashtag <btstsiy@hotmail.com>
2023-07-15 16:57:46 +0300

    simple hash database

---
+++ src/data/hashes.js
@@ -0,0 +1,28 @@
+// id, hashes
+export var hashes = [
+	{
+		id: 0,
+		hash: 372578
+		// deneme
+	},
+	{
+		id: 1,
+		hash: 357603
+		// abcdef
+	},
+	{
+		id: 2,
+		hash: 152745
+		// 12345
+	},
+	{
+		id: 3,
+		hash: 370781
+		// hehele
+	},
+	{
+		id: 4,
+		hash: 457636
+		// hübele
+	}
+]
