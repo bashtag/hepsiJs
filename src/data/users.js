0b3068d3bb8d51c284961e70fcdd7d8d3c223683
bashtag <btstsiy@hotmail.com>
2023-07-15 16:57:58 +0300

    simple users database

---
+++ src/data/users.js
@@ -0,0 +1,48 @@
+// id, firstName, lastName, email, number, birthDate, city
+export var users = [
+	{
+		id: 0,
+		firstName: "Bilal",
+		lastName: "GÖKÇE",
+		email: "btstsiy@gmail.com",
+		number: "01234567890",
+		birthDate: 2000.,
+		city: "Istanbul"
+	},
+	{
+		id: 1,
+		firstName: "Aprikot",
+		lastName: "Ananas",
+		email: "abcdef@gmail.com",
+		number: "02314598760",
+		birthDate: 1995,
+		city: "Istanbul"
+	},
+	{
+		id: 2,
+		firstName: "Angela",
+		lastName: "White",
+		email: "lorem@ipsum.com",
+		number: "93281047560",
+		birthDate: 2013,
+		city: "Istanbul"
+	},
+	{
+		id: 3,
+		firstName: "Deneme",
+		lastName: "Birİki",
+		email: "ipsum@lorem.com",
+		number: "responsive",
+		birthDate: 2001,
+		city: "Istanbul"
+	},
+	{
+		id: 4,
+		firstName: "Lorem",
+		lastName: "İpsum",
+		email: "psum@hot.com",
+		number: "83742916500",
+		birthDate: 1998,
+		city: "Istanbul"
+	}
+]
