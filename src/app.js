d380942568484b93b9e299238c6b225664efe556
bashtag <btstsiy@hotmail.com>
2023-07-16 16:25:03 +0300

    main updated

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import SignBtn from "./components/signBtn/sign.js";
 import SwitchBtn from "./components/signRadioBtn/switch.js";
-import Hash from "./modules/authantication/hash.js";
+import JsLogger from "./crossCuttingConcerns/logging/logger.js";
 import Page from "./modules/page.js";
+import CustomerService from "./services/CustomerService.js"
 
 // Page Action Infos
 let	pageActionInfo = new Page();
@@ -9,5 +10,11 @@ let	pageActionInfo = new Page();
 // Radio Button Attribute
 let	radioBtnAttribute = new SwitchBtn(pageActionInfo);
 
+// Logger
+let	jsLogger = new JsLogger();
+
+// Customer Service
+let	customerService = new CustomerService(jsLogger);
+
 // Sign Button
-// let	signButtonAction = new SignBtn(pageActionInfo);
+let	signButtonAction = new SignBtn(pageActionInfo, customerService, jsLogger);
