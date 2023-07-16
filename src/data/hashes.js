74a42574f291cf613d3abdd68554f34c1ce66864
bashtag <btstsiy@hotmail.com>
2023-07-16 16:26:19 +0300

    hash values for some inputs

--- src/data/hashes.js
+++ src/data/hashes.js
@@ -2,27 +2,27 @@
 export var hashes = [
 	{
 		id: 0,
-		hash: 372578
+		hashPass: 372578
 		// deneme
 	},
 	{
 		id: 1,
-		hash: 357603
+		hashPass: 357603
 		// abcdef
 	},
 	{
 		id: 2,
-		hash: 152745
+		hashPass: 152745
 		// 12345
 	},
 	{
 		id: 3,
-		hash: 370781
+		hashPass: 370781
 		// hehele
 	},
 	{
 		id: 4,
-		hash: 457636
+		hashPass: 457636
 		// hübele
 	}
 ]
