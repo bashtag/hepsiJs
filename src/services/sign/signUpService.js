60a5738357e00a1be5937c7b3a667c53e1840200
bashtag <btstsiy@hotmail.com>
2023-07-16 16:28:59 +0300

    constructor parameters

--- src/services/sign/signUpService.js
+++ src/services/sign/signUpService.js
@@ -1,5 +1,15 @@
+import BaseLogger from "../../crossCuttingConcerns/logging/baselogger.js";
+import CustomerService from "../CustomerService.js";
+
 export default class	SignUpService {
-	constructor(customerService, loggerService) {
+	/**
+	 * 
+	 * @param {CustomerService} customerService 
+	 * @param {BaseLogger} loggerService 
+	 * @param {string} inputText 
+	 * @param {number} isNumber 
+	 */
+	constructor(customerService, loggerService, inputText, isNumber) {
 
 	}
 }
