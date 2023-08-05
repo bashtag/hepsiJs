154bc8918592c98438defbd57728586e8b332481
bashtag <btstsiy@hotmail.com>
2023-08-05 21:44:38 +0300

    just skeleton

--- src/services/sign/signUpService.js
+++ src/services/sign/signUpService.js
@@ -1,7 +1,9 @@
 import BaseLogger from "../../crossCuttingConcerns/logging/baselogger.js";
 import CustomerService from "../CustomerService.js";
+import SignService from "./SignService.js";
 
-export default class	SignUpService {
+export default class	SignUpService extends SignService {
+	
 	/**
 	 * 
 	 * @param {CustomerService} customerService 
@@ -10,6 +12,14 @@ export default class	SignUpService {
 	 * @param {number} isNumber 
 	 */
 	constructor(customerService, loggerService, inputText, isNumber) {
+		super(customerService, loggerService, inputText, isNumber)
+	}
 
+	/**
+	 * Auth scene for sign up which including a form.
+	 * Pure Virtual Method.
+	 */
+	generateAuthScene() {
+		console.log("sign up")
 	}
 }
