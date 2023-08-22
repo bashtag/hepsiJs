4e592a28f3d35e24031dadd819fa369e5141b20f
bashtag <btstsiy@hotmail.com>
2023-08-22 18:03:00 +0300

    input log, emal match log, successful login log

--- src/crossCuttingConcerns/logging/baselogger.js
+++ src/crossCuttingConcerns/logging/baselogger.js
@@ -12,6 +12,27 @@ export default class	BaseLogger {
 	 * @param {object} msg 
 	 */
 	addLog(msg) {
-		console.log("(Add)", this.logger, ":", msg)
+		console.log("(Add)", this.logger, ":", msg);
+	}
+
+	/**
+	 * Log for input
+	 */
+	inputLog(msg) {
+		console.log("(Wrong Input)", this.logger, ":", msg);
+	}
+
+	/**
+	 * Log if email matches
+	 */
+	emailMatchLog(msg) {
+		console.log("(Email Matches)", this.logger, ":", msg);
+	}
+
+	/**
+	 * Log for successful login
+	 */
+	successfulLoginLog(msg) {
+		console.log("(Successful Login)", this.logger, ":", msg);
 	}
 }
